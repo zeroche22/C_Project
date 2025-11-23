fc0dfe275c9ea9b951e0cb0454bb203364a2d882
zeroche22 <manzhos.yaroslav22@gmail.com>
2025-11-23 17:50:41 +0200

    rework

--- server/include/analysis.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANALYSIS_H
-#define ANALYSIS_H
-
-#include <stddef.h>
-
-char *extract_context(const char *message, size_t position, size_t radius);
-void save_match_context(const char *ip, const char *message);
-
-#endif // ANALYSIS_H
--- server/include/server.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-int start_tcp_server(void);
-
-#endif // SERVER_H
